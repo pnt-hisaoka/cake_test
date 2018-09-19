996fab47e28f3fd0307558f2be9e5282e0f395b3
pnthisaoka <hisaoka@p-nt.com>
2018-09-19 11:28:01 +0000

    change

--- config/app.default.php
+++ config/app.default.php
@@ -257,7 +257,7 @@ return [
             //'port' => 'non_standard_port_number',
             'username' => 'myuser',
             'password' => 'password',
-            'database' => 'circle_test ',
+            'database' => 'circle_test',
             //'encoding' => 'utf8mb4',
             /*
              * You do not need to set this flag to use full utf-8 encoding (internal default since CakePHP 3.6).
@@ -301,7 +301,7 @@ return [
             //'port' => 'non_standard_port_number',
             'username' => 'myuser',
             'password' => 'password',
-            'database' => 'circle_test ',
+            'database' => 'circle_test',
             //'encoding' => 'utf8mb4',
             'timezone' => 'UTC',
             'cacheMetadata' => true,
