d9a2a55c00e1d3cd843b9b572f9df7288e70080a
pnthisaoka <hisaoka@p-nt.com>
2018-09-20 10:03:57 +0000

    修正

--- tests/TestCase/Model/Table/UsersTableTest.php
+++ tests/TestCase/Model/Table/UsersTableTest.php
@@ -58,7 +58,7 @@ class UsersTableTest extends TestCase
      */
     public function testInitialize()
     {   
-        $this->assertEquals(1, 1);
+        $this->assertEquals(2, 2);
         $this->markTestIncomplete('Not implemented yet.');
     }
 
